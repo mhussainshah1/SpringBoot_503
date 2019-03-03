c3eba510822457ac8587ab9cd24d38d2c6711778
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-02 21:37:23 -0500

    Add validation

--- src/main/java/com/example/demo/Actor.java
+++ src/main/java/com/example/demo/Actor.java
@@ -4,15 +4,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Actor {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @NotNull
+    @Size(min=4)
     private String name;
+
+    @NotNull
+    @Size(min=4)
     private String realname;
+
     private String headshot;//contains URL
+
+    public Actor() {
+
+    }
+
+    public Actor(@NotNull @Size(min = 4) String name, @NotNull @Size(min = 4) String realname, String headshot) {
+        this.name = name;
+        this.realname = realname;
+        this.headshot = headshot;
+    }
 
     public long getId() {
         return id;
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,10 +4,11 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.Map;
 
@@ -28,14 +29,17 @@ public class HomeController {
     @GetMapping("/add")
     public String newActor(Model model){
         model.addAttribute("actor", new Actor());
+        System.out.println("Get Mapping called");
         return "form";
     }
 
-    @PostMapping("/add")
-    public String processActor(@ModelAttribute Actor actor,
-                               @RequestParam("file") MultipartFile file){
-        if(file.isEmpty()){
-            return "redirect:/add";
+    @PostMapping("/process")
+    public String processActor(@Valid Actor actor,
+                               BindingResult result,
+                               @RequestParam("file") MultipartFile file ){
+        System.out.println("Post Mapping called");
+        if(result.hasErrors() || file.isEmpty()){
+            return "form";//"redirect:/add";
         }
         try{
             Map uploadResult = cloudc.upload(
@@ -46,7 +50,7 @@ public class HomeController {
             // values (<<name>>,<<realname>>,<<headshot>>) where id = actor.id
         } catch (IOException e){
             e.printStackTrace();;
-            return "redirect:/add";
+            return "redirect:/form";
         }
         return "redirect:/";
     }
